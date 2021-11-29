2a64b398e68c3ce003ee61d863e2cbd6756168f7
Hadenix <vsegda_online_007@bk.ru>
2021-11-29 23:18:42 +0300

    a11_tab: Initial TWRP device tree * UMIDIGI/A11_Tab/A11_Tab:11/RP1A.200720.011/1630047031:user/release-keys
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_A11_Tab.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_A11_Tab-user \
+    omni_A11_Tab-userdebug \
+    omni_A11_Tab-eng
---
+++ omni_A11_Tab.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from A11_Tab device
+$(call inherit-product, device/umidigi/A11_Tab/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := A11_Tab
+PRODUCT_NAME := omni_A11_Tab
+PRODUCT_BRAND := UMIDIGI
+PRODUCT_MODEL := MT01
+PRODUCT_MANUFACTURER := umidigi
